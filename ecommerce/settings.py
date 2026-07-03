3af437762e31dc43ce718ee0508416cf058891ed
Modi Ruchin <modiruchin@gmail.com>
2026-07-03 20:47:25 +0530

    Improve production settings and security

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,9 @@ import os
 import cloudinary
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$dwp*%^#77fh!^azkl^87f+7&m%%n_f&p6je&oe@8o_8(xp(rt'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "fruit-basket-nrnm.onrender.com",
+]
 
 
 # Application definition
