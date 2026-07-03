03560b5c2372851615c6fa2a024322e41482bd7a
Modi Ruchin <modiruchin@gmail.com>
2026-07-03 18:32:19 +0530

    configure cloudinary uploadaer

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 import dj_database_url
 from pathlib import Path
 
